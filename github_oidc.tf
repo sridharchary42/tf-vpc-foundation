d0c62c9fc7d5082975ac7a6562b8d68b5b382181
SridharcharyMalloju <smalloju@aum.edu>
2026-08-02 17:56:05 -0400

    Fix GitHub OIDC trust policy: account for immutable repo IDs and session tagging

--- github_oidc.tf
+++ github_oidc.tf
@@ -24,13 +24,13 @@ resource "aws_iam_role" "github_actions" {
       Principal = {
         Federated = aws_iam_openid_connect_provider.github_actions.arn
       }
-      Action = "sts:AssumeRoleWithWebIdentity"
+      Action = ["sts:AssumeRoleWithWebIdentity", "sts:TagSession"]
       Condition = {
         StringEquals = {
           "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
         }
         StringLike = {
-          "token.actions.githubusercontent.com:sub" = "repo:sridharchary42/tf-vpc-foundation:*"
+          "token.actions.githubusercontent.com:sub" = "repo:sridharchary42@*/tf-vpc-foundation@*:*"
         }
       }
     }]
